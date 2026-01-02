e25d21cbbd14161ea2e9417c627e83390a570fd9
Krish Bhollar Tyagi <krishbtyagi@gmail.com>
2026-01-02 20:08:18 +0530

    clock using useRef

--- src/App.jsx
+++ src/App.jsx
@@ -1,58 +1,25 @@
-import {BrowserRouter, Routes, Route, Link, useNavigate, Outlet } from "react-router-dom"
+import { useRef, useState } from 'react';
 
 function App() {
-  return <div>
-    <BrowserRouter>      
-      <Routes>
-        <Route path="/" element={<Layout/>} />
-          <Route path="/neet/online-coaching-class-11" element={<Class11Program />} />
-          <Route path="/neet/online-coaching-class-12" element={<Class12Program />} />
-          <Route path="*" element={<ErrorPage />} />
-          <Route path="/" element={<Landing />} />
-      </Routes>
-    </BrowserRouter>
-  </div>
-}
+  const [currentcount, setCurrentCount] = useState(1);
+  const  timer = useRef();
 
-function Layout() {
-  return <div style={{height: "100vh"}}>
-      <Link to ="/">Allen </Link>
-      <Link to ="/neet/online-coaching-class-11">class11 </Link>
-      <Link to ="/neet/online-coaching-class-12">class12 </Link>
-      <div style={{height: "90vh"}}>
-        <Outlet />
-      </div>
-    footer
-  </div>
-}
-
-function Landing() {
-  return <div>
-    welcome to allen
-  </div>
-}
-
-function Class11Program() {
-  return <div>
-    hi class11
-  </div>
-}
-
-function Class12Program() {
-  const navigate = useNavigate();
-
-  function redirectUser(){
-    navigate("/")
+  function startClock(){
+    let value = setInterval(function(){
+      setCurrentCount(c => c+1);
+    },1000)
+    timer.current = value;
   }
-  return <div>
-    hi class12
-    <button onClick={redirectUser}>go back to landing page</button>
-  </div>
-}
+  
+  function stopClock(){
+    clearInterval(timer.current);
+  }
 
-function ErrorPage(){ 
   return <div>
-    sorry, page not found
+    {currentcount}
+    <br />
+    <button onClick={startClock}>Start</button>
+    <button onClick={stopClock}>Stop</button>
   </div>
 }
 export default App
