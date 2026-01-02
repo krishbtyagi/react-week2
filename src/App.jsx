fecf227e63cd2ab2111a41b5a9fbd16477799dfa
Krish Bhollar Tyagi <krishbtyagi@gmail.com>
2026-01-02 15:29:51 +0530

    routing & layouts

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,58 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import {BrowserRouter, Routes, Route, Link, useNavigate, Outlet } from "react-router-dom"
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+  return <div>
+    <BrowserRouter>      
+      <Routes>
+        <Route path="/" element={<Layout/>} />
+          <Route path="/neet/online-coaching-class-11" element={<Class11Program />} />
+          <Route path="/neet/online-coaching-class-12" element={<Class12Program />} />
+          <Route path="*" element={<ErrorPage />} />
+          <Route path="/" element={<Landing />} />
+      </Routes>
+    </BrowserRouter>
+  </div>
 }
 
+function Layout() {
+  return <div style={{height: "100vh"}}>
+      <Link to ="/">Allen </Link>
+      <Link to ="/neet/online-coaching-class-11">class11 </Link>
+      <Link to ="/neet/online-coaching-class-12">class12 </Link>
+      <div style={{height: "90vh"}}>
+        <Outlet />
+      </div>
+    footer
+  </div>
+}
+
+function Landing() {
+  return <div>
+    welcome to allen
+  </div>
+}
+
+function Class11Program() {
+  return <div>
+    hi class11
+  </div>
+}
+
+function Class12Program() {
+  const navigate = useNavigate();
+
+  function redirectUser(){
+    navigate("/")
+  }
+  return <div>
+    hi class12
+    <button onClick={redirectUser}>go back to landing page</button>
+  </div>
+}
+
+function ErrorPage(){ 
+  return <div>
+    sorry, page not found
+  </div>
+}
 export default App
